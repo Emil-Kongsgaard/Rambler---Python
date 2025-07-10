981f0cf49be439560f65d96ad4b60ee66280622f
Emil Kongsgaard <ebkoprivat@outlook.dk>
2025-07-10 15:48:25 +0200

    added __init__.py files and created a test pacakge in order to run test-scripts.

--- src/persist_event.py
+++
@@ -1,5 +0,0 @@
-#Events must be persisted as .txt files in a directory that is part of the game files.
-# For now the directory will be a subdirectory under the main project directory. 
-# the directory should be named "c:/...../game_events". 
-# An event should be given a descriptive name and suffixed by a 4 digit counter that functions as an ID-number eg: "first_event_0001.txt"
-# there might be an concurrency issue in the future. It might be worth exploring using a Datebase. For now it will not change.
